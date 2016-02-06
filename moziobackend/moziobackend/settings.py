8a08d01c36c0f1d72ce5e9f1deb53c04dd14e0fc
Manuel Kaufmann <humitos@gmail.com>
2016-02-06 09:46:36 -0500

    Remove unnecessary things

--- moziobackend/moziobackend/settings.py
+++ moziobackend/moziobackend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'django_extensions',
     'rest_framework',
     'rest_framework_gis',
     'serviceareas',
